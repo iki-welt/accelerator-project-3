812e71cc71a195b0f1faa08c8b3ac25c56d02d34
iki_welt <iki.welt@ya.ru>
2024-10-01 02:47:29 +0300

    add news desktop slider script

--- source/js/news-slider.js
+++ source/js/news-slider.js
@@ -4,6 +4,7 @@ import 'swiper/css';
 import 'swiper/css/grid';
 
 const sliderContainer = document.querySelector('.news__slider');
+const sliderWrapper = document.querySelector('.news__slider-wrapper');
 const Slides = sliderContainer.querySelectorAll('.news__slide');
 
 const changeSlideHeight = (slide, index) => {
@@ -18,6 +19,18 @@ const changeSlideHeight = (slide, index) => {
 
 const setSlides = () => {
   Slides.forEach((slide, index) => changeSlideHeight(slide, index));
+};
+
+const setFirstSlide = (currentSlide, activeSlide) => {
+  currentSlide.querySelector('.news-card__pic-wrapper').classList.remove('news-card__pic-wrapper--first');
+  currentSlide.querySelector('.news-card__title').classList.remove('news-card__title--first');
+  currentSlide.querySelector('.news-card__descr-wrapper').classList.remove('news-card__descr-wrapper--first');
+  currentSlide.classList.remove('news-card--first');
+
+  activeSlide.querySelector('.news-card__pic-wrapper').classList.add('news-card__pic-wrapper--first');
+  activeSlide.querySelector('.news-card__title').classList.add('news-card__title--first');
+  activeSlide.querySelector('.news-card__descr-wrapper').classList.add('news-card__descr-wrapper--first');
+  activeSlide.classList.add('news-card--first');
 };
 
 const newsSlider = new Swiper('.news__slider', {
@@ -55,14 +68,39 @@ const newsSlider = new Swiper('.news__slider', {
       allowTouchMove: 'true',
     },
     1440: {
-      // slidesPerView: 3,
-      // spaceBetween: 32,
-      // allowTouchMove: 'false',
+      slidesPerView: 'auto',
+      spaceBetween: 32,
+      grid: false,
+      allowTouchMove: 'false',
     },
   },
 },
 );
 
+const setStartPagination = () => {
+  if (newsSlider.pagination.bullets.length > 4) {
+    newsSlider.pagination.bullets.forEach((bullet, index) => {
+      if (index > 3) {
+        bullet.style.display = 'none';
+      }
+    });
+  }
+  // const amounSlides = newsSlider.slides.length;
+  // console.log(newsSlider.pagination);
+  // console.log(amounSlides);
+};
+
+const changeFirstSlideWidth = () => {
+  const activeSlide = sliderWrapper.querySelector('.swiper-slide-active');
+  if (!activeSlide.matches('.news-card--first')) {
+    const currentSlide = sliderWrapper.querySelector('.news-card--first');
+    setFirstSlide(currentSlide, activeSlide);
+    newsSlider.update();
+  }
+};
+
 newsSlider.on('afterInit', setSlides());
+newsSlider.on('afterInit', setStartPagination());
+newsSlider.on('slideChangeTransitionStart', changeFirstSlideWidth);
 
 export { newsSlider };
